ce49a42d51ac6b9143b5563e65ee1233cc10746b
Nohely Scarleth Villegas Menendez <107574681+NohelyVillegas@users.noreply.github.com>
2025-01-13 23:14:08 -0500

    Delete micro_transaccion directory

--- micro_transaccion/src/main/java/com/espe/micro_transaccion/repositories/TransaccionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.espe.micro_transaccion.repositories;
-
-
-import com.espe.micro_transaccion.models.entities.Transaccion;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TransaccionRepository extends CrudRepository<Transaccion, Long> {
-}
